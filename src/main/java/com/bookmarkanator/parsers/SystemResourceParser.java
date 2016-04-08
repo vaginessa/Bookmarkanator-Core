a453dd78bc84b333aac871bbcb5bedc71cd9cb51
flightx31 <flightx31@gmail.com>
2016-04-07 20:20:34 -0600

    Added parser switch cases for creating necessary objects.

--- src/main/java/com/bookmarkanator/parsers/SystemResourceParser.java
+++ src/main/java/com/bookmarkanator/parsers/SystemResourceParser.java
@@ -3,6 +3,11 @@ package com.bookmarkanator.parsers;
 import java.io.*;
 import java.util.*;
 import javax.xml.stream.*;
+
+import com.bookmarkanator.resourcetypes.BasicResource;
+import com.bookmarkanator.resourcetypes.CustomFileFilter;
+import com.bookmarkanator.resourcetypes.DefaultSystemResource;
+import com.bookmarkanator.resourcetypes.TerminalResource;
 import com.bookmarkanator.settings.*;
 
 public class SystemResourceParser
@@ -57,8 +62,12 @@ public class SystemResourceParser
 
     }
 
-    private List<SystemType> systemTypes;
+    private Settings currentSettings;
     private SystemType currentSystem;
+    private BasicResource currentBasicResource;
+    private DefaultSystemResource currentDefaultSystemResource;
+    private CustomFileFilter currentCustomFileFilter;
+    private TerminalResource currentTerminalResource;
     private XMLStreamReader reader;
     private Stack<Tags> stateStack;
     private Stack<StringBuilder> charsStack;
@@ -77,14 +86,13 @@ public class SystemResourceParser
         }
     }
 
-    public List<SystemType> parse(Reader in)
+    public Settings parse(Reader in)
         throws XMLStreamException
     {
         XMLInputFactory xif = XMLInputFactory.newInstance();
         xif.setProperty(XMLInputFactory.IS_NAMESPACE_AWARE, false);
         reader = xif.createXMLStreamReader(in);
 
-        systemTypes = new ArrayList<SystemType>();
 
         stateStack = new Stack<Tags>();
         charsStack = new Stack<StringBuilder>();
@@ -110,7 +118,7 @@ public class SystemResourceParser
             }
         }
 
-        return systemTypes;
+        return currentSettings;
     }
 
     public void startTag(String currentTag)
@@ -122,9 +130,19 @@ public class SystemResourceParser
         {
             case root:
                 state =match(state, Tags.settings, currentTag);
+                if (state==Tags.settings)
+                {
+                    currentSettings = new Settings();
+                    //todo set attributes
+                }
                 break;
             case settings:
                 state =match(state, Tags.system, currentTag);
+                if (state==Tags.system)
+                {
+                    currentSystem = new SystemType();
+                    //todo set attributes
+                }
                 break;
             case system:
                 state =match(state, Tags.basicresource, currentTag);
@@ -133,6 +151,31 @@ public class SystemResourceParser
                 state =match(state, Tags.web, currentTag);
                 state =match(state, Tags.terminalresource, currentTag);
                 state =match(state, Tags.customclass, currentTag);
+                if (state==Tags.basicresource)
+                {
+                    currentBasicResource = new BasicResource();
+                    //todo set attributes
+                }
+                else if (state==Tags.filebrowser)
+                {
+
+                }
+                else if (state==Tags.fileeditor)
+                {
+
+                }
+                else if (state==Tags.web)
+                {
+
+                }
+                else if (state==Tags.terminalresource)
+                {
+
+                }
+                else if (state==Tags.customclass)
+                {
+
+                }
                 break;
             case basicresource:
                 state =match(state, Tags.name, currentTag);
@@ -142,8 +185,23 @@ public class SystemResourceParser
                 state =match(state, Tags.classpointer, currentTag);
                 state =match(state, Tags.parameter, currentTag);
             case terminalresource:
+                state =match(state, Tags.name, currentTag);
+                state =match(state, Tags.text, currentTag);
+                state =match(state, Tags.precommand, currentTag);
+                state =match(state, Tags.postcommand, currentTag);
+                break;
             case web:
+                state =match(state, Tags.name, currentTag);
+                state =match(state, Tags.text, currentTag);
+                state =match(state, Tags.precommand, currentTag);
+                state =match(state, Tags.postcommand, currentTag);
+                break;
             case fileeditor:
+                state =match(state, Tags.name, currentTag);
+                state =match(state, Tags.text, currentTag);
+                state =match(state, Tags.precommand, currentTag);
+                state =match(state, Tags.postcommand, currentTag);
+                break;
             case filebrowser:
                 state =match(state, Tags.name, currentTag);
                 state =match(state, Tags.text, currentTag);
