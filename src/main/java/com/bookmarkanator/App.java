d688b497211bb864a092de62680b2f0bc1c281f1
flightx31 <flightx31@gmail.com>
2016-04-12 17:43:20 -0600

    Begun adding system tray support.

--- src/main/java/com/bookmarkanator/App.java
+++ src/main/java/com/bookmarkanator/App.java
@@ -1,7 +1,6 @@
 package com.bookmarkanator;
 
-import java.io.*;
-import com.bookmarkanator.parsers.*;
+import com.bookmarkanator.gui.*;
 
 /**
  * Hello world!
@@ -12,24 +11,6 @@ public class App
     public static void main( String[] args )
 		throws Exception
 	{
-		SystemResourceParser p = new SystemResourceParser();
-		File t = new File("");
-		try
-		{
-			System.out.println(t.getCanonicalPath().toString());
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-		File f = new File("/Users/lloyd1/Projects/Bookmark-anator/src/main/system_resource_settings.xml");
-		try
-		{
-			p.parse(f);
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
+		MainFrame mf = new MainFrame();
 	}
 }
