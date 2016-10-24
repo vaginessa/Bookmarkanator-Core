f4b05fe2e60a6b51dabdf7cf0cebbd4b10b02321
Micah Lloyd <m.lloyd.1@elsevier.com>
2016-10-24 10:33:14 -0600

    Starting over... Removed all the old stuff. Starting new with a simpler approach.

---
+++ src/main/java/com/bookmarkanator/Bookmark.java
@@ -0,0 +1,10 @@
+package com.bookmarkanator;
+
+import java.util.*;
+
+public class Bookmark {
+    private String type;
+    private String name;
+    private UUID id;
+    private String theText;
+}
--- src/main/java/com/bookmarkanator/interfaces/XMLWritable.java
+++
@@ -1,6 +0,0 @@
-package com.bookmarkanator.interfaces;
-
-public interface XMLWritable
-{
-    void toXML(StringBuilder sb, String prependTabs);
-}
